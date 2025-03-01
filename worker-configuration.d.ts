877682770bb55011a0eaa2f3b8e31f884e4e5ad3
Mohamad Oday <56949948+MohamadOday@users.noreply.github.com>
2025-03-01 12:35:21 +0300

    Update worker-configuration.d.ts

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -1,6 +1,6 @@
 interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	MY_KV_NAMESPACE: 7a7e53b0adb44edab12d4e24bdde376c;
+	V2RAY_WORKER_KV_ID: 7a7e53b0adb44edab12d4e24bdde376c;
 	KV_NAMESPACE: 7a7e53b0adb44edab12d4e24bdde376c;
 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
 	// MY_DURABLE_OBJECT: DurableObjectNamespace;
